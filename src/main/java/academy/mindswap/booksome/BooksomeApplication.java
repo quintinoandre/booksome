1ad6738a15f9713f163ae6288653c0e719309a2f
André Quintino <andre.quintino@mindswap.academy>
2022-09-23 15:29:10 +0100

    first commit

---
+++ src/main/java/academy/mindswap/booksome/BooksomeApplication.java
@@ -0,0 +1,13 @@
+package academy.mindswap.booksome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksomeApplication.class, args);
+	}
+
+}
