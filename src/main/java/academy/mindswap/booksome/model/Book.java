6af26d6d42935e74b4c56d97c63ca87e1bbedb40
lygia <lygia.garrido@mindswap.academy>
2022-09-23 19:12:03 +0100

    feat: implement user and book models

--- src/main/java/academy/mindswap/booksome/model/Book.java
+++ src/main/java/academy/mindswap/booksome/model/Book.java
@@ -1,4 +1,27 @@
 package academy.mindswap.booksome.model;
 
-public class Book {
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+@Document
+public class Book implements Serializable {
+
+    @Id
+    private String id;
+    private String title;
+    private String[] authors;
+    private String category;
+    private Integer isbn;
+    private String description;
+    private Date publishedDate;
+    private String publisher;
+
+
 }
--- src/main/java/academy/mindswap/booksome/model/User.java
+++ src/main/java/academy/mindswap/booksome/model/User.java
@@ -1,4 +1,25 @@
 package academy.mindswap.booksome.model;
 
-public class User {
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@Document
+public class User implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
 }
