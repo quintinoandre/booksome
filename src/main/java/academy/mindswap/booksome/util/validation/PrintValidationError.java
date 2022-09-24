2f1c54235be372e85c75635e6e10bd3251bc0941
André Quintino <andre.quintino@mindswap.academy>
2022-09-24 11:14:50 +0100

    feat: implement request body fields validation

--- src/main/java/academy/mindswap/booksome/util/validation/PrintValidationError.java
+++ src/main/java/academy/mindswap/booksome/util/validation/PrintValidationError.java
@@ -1,4 +1,28 @@
 package academy.mindswap.booksome.util.validation;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrintValidationError {
+    private PrintValidationError() {
+    }
+
+    public static ResponseEntity<Map<String, String>> printValidationError(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+
+        bindingResult.getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
