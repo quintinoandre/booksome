e2a5e99536618121a8a38c0c25ec21a2051c4944
lygia <lygia.garrido@mindswap.academy>
2022-09-24 16:06:14 +0100

    feat (converter): add book and user converter methods using model mapper

--- src/main/java/academy/mindswap/booksome/converter/BookConverter.java
+++ src/main/java/academy/mindswap/booksome/converter/BookConverter.java
@@ -1,4 +1,25 @@
 package academy.mindswap.booksome.converter;
 
+import academy.mindswap.booksome.dto.book.BookDto;
+import academy.mindswap.booksome.dto.book.SaveBookDto;
+
+import academy.mindswap.booksome.model.Book;
+import org.modelmapper.ModelMapper;
+
 public class BookConverter {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static BookDto convertBookToBookDto(Book book){
+        return modelMapper.map(book, BookDto.class);
+    }
+    public static Book convertBookDtoToBook(BookDto bookDto){
+        return modelMapper.map(bookDto, Book.class);
+    }
+
+    public static Book convertSaveBookDtoToBook(SaveBookDto saveBookDto){
+        return modelMapper.map(saveBookDto, Book.class);
+    }
+
+
+
 }
--- src/main/java/academy/mindswap/booksome/converter/UserConverter.java
+++ src/main/java/academy/mindswap/booksome/converter/UserConverter.java
@@ -1,4 +1,27 @@
 package academy.mindswap.booksome.converter;
 
+import academy.mindswap.booksome.dto.user.SaveUserDto;
+import academy.mindswap.booksome.dto.user.UpdateUserDto;
+import academy.mindswap.booksome.dto.user.UserDto;
+import academy.mindswap.booksome.model.User;
+import org.modelmapper.ModelMapper;
+
 public class UserConverter {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static UserDto convertUserToUserDto (User user){
+        return modelMapper.map(user, UserDto.class);
+
+    }
+    public static User convertUserDtoToUser (UserDto userDto){
+        return modelMapper.map(userDto, User.class);
+    }
+
+    public static User convertUpdateUserDtoToUser(UpdateUserDto updateUserDto){
+        return modelMapper.map(updateUserDto, User.class);
+    }
+
+    public static User convertSaveUserDtoToUser(SaveUserDto saveUserDto){
+        return modelMapper.map(saveUserDto, User.class);
+    }
 }
