c3ae37da8790f76ab0f9a48dd0422df2ee0d3e8d
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-24 22:10:11 +0100

    feat (controller): create book Controller

--- src/main/java/academy/mindswap/booksome/controller/BookComtrollerImpl.java
+++ src/main/java/academy/mindswap/booksome/controller/BookComtrollerImpl.java
@@ -1,4 +1,31 @@
 package academy.mindswap.booksome.controller;
 
-public class BookComtrollerImpl {
+import academy.mindswap.booksome.dto.book.BookDto;
+import academy.mindswap.booksome.service.implementation.BookServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookControllerImpl {
+
+    private final BookServiceImpl bookService;
+
+    public BookControllerImpl(BookServiceImpl bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> findAll(@RequestParam Map<String, String> allParams) {
+        List<BookDto> bookList = bookService.findAll(allParams);
+        return new ResponseEntity<>(bookList, HttpStatus.OK);
+    }
 }
+
