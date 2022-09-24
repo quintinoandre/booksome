1abb85331e82e8670e6ff1fa3192912dde2567fa
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-24 23:37:58 +0100

    feat (service): add method findAll to bookService Interface

--- src/main/java/academy/mindswap/booksome/service/interfaces/BookService.java
+++ src/main/java/academy/mindswap/booksome/service/interfaces/BookService.java
@@ -1,4 +1,10 @@
 package academy.mindswap.booksome.service.interfaces;
 
+import academy.mindswap.booksome.dto.book.BookDto;
+
+import java.util.List;
+import java.util.Map;
+
 public interface BookService {
+    public List<BookDto> findAll(Map<String, String> allParams);
 }
