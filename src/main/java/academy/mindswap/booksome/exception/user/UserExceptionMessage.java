a7f9c327b1d70c688cd798f76ef2c8d38fd5d4e4
André Quintino <andre.quintino@mindswap.academy>
2022-09-25 23:16:01 +0100

    feat(exception): implement user bad request exception

--- src/main/java/academy/mindswap/booksome/exception/user/UserExceptionMessage.java
+++ src/main/java/academy/mindswap/booksome/exception/user/UserExceptionMessage.java
@@ -6,4 +6,6 @@ public final class UserExceptionMessage {
 
     public static final String USER_NOT_FOUND = "user not found";
     public static final String USERS_NOT_FOUND = "no user found";
+    public static final String USER_NULL = "user object is null";
+    public static final String EMAIL_ALREADY_EXISTS = "email already exists";
 }
