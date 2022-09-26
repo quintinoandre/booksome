214f03de43e7f48fe98344d7a50ff429344c9b49
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-26 19:54:56 +0100

    feat (book): add @AllArgsConstructor and @NoArgsConstructor

--- src/main/java/academy/mindswap/booksome/model/Book.java
+++ src/main/java/academy/mindswap/booksome/model/Book.java
@@ -1,7 +1,9 @@
 package academy.mindswap.booksome.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(value = "books")
 public class Book implements Serializable {
 
