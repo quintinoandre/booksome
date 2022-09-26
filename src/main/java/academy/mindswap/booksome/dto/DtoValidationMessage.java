a72cc26c3284525168e58b209c9ec980a74faefc
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-26 19:49:39 +0100

    feat (message): set messages and constants

---
+++ src/main/java/academy/mindswap/booksome/dto/DtoValidationMessage.java
@@ -0,0 +1,21 @@
+package academy.mindswap.booksome.dto;
+
+public final class DtoValidationMessage {
+
+    private DtoValidationMessage() {
+    }
+
+    public static final String TITLE_MANDATORY = "Title is mandatory.";
+    public static final String ISBN_MANDATORY = "ISBN is mandatory.";
+    public static final String AUTHOR_MANDATORY = "Author is mandatory.";
+    public static final String CATEGORY_MANDATORY = "Category is mandatory.";
+    public static final String DESCRIPTION_MANDATORY = "Description is mandatory.";
+    public static final String PUBLISHED_DATE_MANDATORY = "Published date is mandatory.";
+    public static final String PUBLISHER_MANDATORY = "Publisher is mandatory.";
+
+    public static final String NAME_MANDATORY = "Name is mandatory.";
+    public static final String EMAIL_MANDATORY = "Email must be valid.";
+    public static final String ROLE_MANDATORY = "At least one role is mandatory.";
+    public static final String USERNAME_MANDATORY = "Username is mandatory.";
+    public static final String PASSWORD_MANDATORY = "Password is mandatory.";
+}
--- src/main/java/academy/mindswap/booksome/util/user/UserMessage.java
+++ src/main/java/academy/mindswap/booksome/util/user/UserMessage.java
@@ -4,10 +4,11 @@ public final class UserMessage {
     private UserMessage() {
     }
 
-    public static final String USERS_FOUND = "users found successfully";
-    public static final String USER_FOUND = "user found successfully";
-    public static final String USER_SAVED = "user saved successfully";
-    public static final String ROLES_ASSIGN = "roles successfully assigned to user";
-    public static final String USER_UPDATED = "user updated successfully";
-    public static final String USER_DELETED = "user deleted successfully";
+    public static final String USERS_FOUND = "Users successfully found.";
+    public static final String USER_FOUND = "User successfully found.";
+    public static final String USER_SAVED = "User successfully saved.";
+    public static final String ROLES_ASSIGN = "Roles successfully assigned to user.";
+    public static final String USER_UPDATED = "User successfully updated.";
+    public static final String USER_DELETED = "User successfully deleted.";
+    public static final String ADDED_FAVORITE_BOOK = "The book has been successfully added to your favorites.";
 }
