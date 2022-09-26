713ba68a0da45b9b5d43e6d69f6339a2003165eb
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-26 19:56:41 +0100

    feat (service): add methods in service to check if book has been saved and save in database

--- src/main/java/academy/mindswap/booksome/repository/BookRepository.java
+++ src/main/java/academy/mindswap/booksome/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package academy.mindswap.booksome.repository;
+
 import academy.mindswap.booksome.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
@@ -11,4 +13,7 @@ public interface BookRepository extends MongoRepository<Book, String> {
     @Query("{ $or: [ { title: { $regex: /?0/, $options: 'i' } }, { authors: { $in: [ /^?1/i ] } }, " +
             "{ category: { $in: [ /^?2/i ] } }, { isbn: { $regex: /?3/, $options: 'i' } } ] }")
     List<Book> findAll(String title, String authors, String category, String isbn);
+
+    Boolean existsByIsbn(String isbn);
+    Book findByIsbn(String isbn);
 }
--- src/main/java/academy/mindswap/booksome/service/interfaces/BookService.java
+++ src/main/java/academy/mindswap/booksome/service/interfaces/BookService.java
@@ -1,10 +1,16 @@
 package academy.mindswap.booksome.service.interfaces;
 
+import academy.mindswap.booksome.dto.book.BookClientDto;
 import academy.mindswap.booksome.dto.book.BookDto;
+import academy.mindswap.booksome.dto.book.SaveBookDto;
+import academy.mindswap.booksome.model.Book;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BookService {
+    BookDto save(BookClientDto bookClientDto);
+
     public List<?> findAll(Map<String, String> allParams);
+    public Book findByIsbn(String isbn);
 }
