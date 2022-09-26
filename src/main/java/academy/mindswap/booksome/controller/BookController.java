26d60d1fd44ffd12207b83ad96a6fa087a7358d8
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-26 11:01:19 +0100

    feat (controller): add feat to throw exception if no param sent

--- src/main/java/academy/mindswap/booksome/controller/BookController.java
+++ src/main/java/academy/mindswap/booksome/controller/BookController.java
@@ -1,6 +1,7 @@
 package academy.mindswap.booksome.controller;
 
 import academy.mindswap.booksome.dto.book.BookDto;
+import academy.mindswap.booksome.exception.book.BookBadRequestException;
 import academy.mindswap.booksome.service.implementation.BookServiceImpl;
 import academy.mindswap.booksome.service.interfaces.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import static academy.mindswap.booksome.exception.book.BookExceptionMessage.ALL_PARAMS_NULL;
 
 import java.util.List;
 import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -27,6 +30,7 @@ public class BookController {
 
     @GetMapping
     public ResponseEntity<List<?>> findAll(@RequestParam Map<String, String> allParams) {
+        if (allParams.isEmpty()) throw new BookBadRequestException(ALL_PARAMS_NULL);
         List<?> bookList = bookService.findAll(allParams);
         return new ResponseEntity<>(bookList, HttpStatus.OK);
     }
