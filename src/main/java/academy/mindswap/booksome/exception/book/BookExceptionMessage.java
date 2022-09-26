4a098ea64cc5351b1863de4d03155b3596236e98
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-26 14:42:47 +0100

    feat (exception): add class for String constants

--- src/main/java/academy/mindswap/booksome/exception/book/BookExceptionMessage.java
+++ src/main/java/academy/mindswap/booksome/exception/book/BookExceptionMessage.java
@@ -1,9 +1,11 @@
 package academy.mindswap.booksome.exception.book;
 
 public final class BookExceptionMessage {
-
     private BookExceptionMessage() {}
 
     public static final String ALL_PARAMS_NULL = "At least one parameter is required";
-
+    public static final String INVALID_TITLE = "Please insert a valid book title (eg. Harry Potter)";
+    public static final String INVALID_AUTHOR = "Please insert a valid book author (eg. J. K. Rowling)";
+    public static final String INVALID_CATEGORY = "Please insert a valid book category (eg. Fiction)";
+    public static final String INVALID_ISBN = "Please insert a valid book ISBN (eg. 9789722325332)";
 }
